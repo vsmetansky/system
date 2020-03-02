41591290cb76a99ceee37e5cb24c3212721d5a47
vsmetansky <vladsmetansky@gmail.com>
2020-03-03 01:11:13 +0200

    logging enchanced

--- serial_io/main.c
+++ serial_io/main.c
@@ -8,11 +8,11 @@
 
 #define ARG_NUM 2
 #define RWXR__R__ 0644
-#define BUF_SIZE 256
+#define BUF_SIZE 10
 #define OFFSET 32
 
 int transmit(int a, int b);
-void transform(char *buf, int buf_size);
+void transform_upper(char *buf, int buf_size);
 
 int main(int argc, char **argv) {
     if (argc < ARG_NUM + 1) {
@@ -21,39 +21,39 @@ int main(int argc, char **argv) {
     }
         
     int i = open(argv[1], O_RDONLY);
-    if (i < 0) logg_term("Error opening file\n");
     int o = open(argv[2], O_WRONLY | O_CREAT, RWXR__R__);
-    if (o < 0) logg_term("Error opening file\n");
+    logg_series_one_term("Error opening file\n", 2, i, o);
 
     int b_num = transmit(i, o);
-    printf("%i bytes were transmitted", &b_num);        
+    printf("%i bytes were transmitted\n", b_num);        
 
-    if (close(i) < 0) logg_term("Error closing file\n");
-    if (close(o) < 0) logg_term("Error closing file\n");
+    logg_series_one_term("Error opening file\n", 2, close(i), close(o));
 
     return EXIT_SUCCESS;
 }
 
-void transform(char *buf, int bytes_num) {
+void transform_upper(char *buf, int bytes_num) {
     for (int i = 0; i < bytes_num; i++) {
         if(isalpha(buf[i])) buf[i] = buf[i] - OFFSET;
     }
 }
 
-int transmit(int a, int b) {
+int transmit(int i, int o) {
     char buf[BUF_SIZE];
     int b_read = 0;
     int b_wrote = 0;
     int b_total = 0;
 
-    while ((b_read = read(a, buf, BUF_SIZE)) >= 0) {
-        transform(buf, b_read);
-        if ((b_wrote = write(b, buf, b_read)) < 0) {
+    while ((b_read = read(i, buf, BUF_SIZE)) > 0) {
+        transform_upper(buf, b_read);
+        if ((b_wrote = write(o, buf, b_read)) < 0) {
             logg("Error writing data");
         } else {
             b_total += b_wrote;
         }    
     }
+
+    if (b_read < 0) logg("Error reading data");
 
     return b_total;
 }
--- serial_io/src/logger.c
+++ serial_io/src/logger.c
@@ -14,3 +14,26 @@ void logg_term(char *message) {
     logg(message);
     exit(EXIT_FAILURE);
 }
+
+void logg_series_one(char *message, int n, ...) {
+    va_list valist;
+    va_start(valist, n);
+    for (int i = 0; i < n; i++) {
+        if (va_arg(valist, int) < 0) {
+            logg(message);
+            break;
+        }
+    }
+    va_end(valist);
+}
+
+void logg_series_one_term(char *message, int n, ...) {
+    va_list valist;
+    va_start(valist, n);
+    for (int i = 0; i < n; i++) {
+        if (va_arg(valist, int) < 0) {
+            logg_term(message);
+        }
+    }
+    va_end(valist);
+}
